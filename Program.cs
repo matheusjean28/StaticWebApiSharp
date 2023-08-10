a33b7b1861c248f83a27a5e9c0e416192c54a3cd
matheusjean28 <matheusjean28@gmail.com>
2023-08-09 21:44:36 -0300

    adjust merge

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ContextPay.PayDb;
+using Models.PayModel;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PayDb>(opt => opt.UseInMemoryDatabase("TodoList"));
@@ -14,17 +15,17 @@ app.MapGet("/todoitems/complete", async (PayDb db) =>
 
 // app.MapGet("/todoitems/{id}", async (int id, PayDb db) =>
 //     await db.Pays.FindAsync(id)
-//         is Pay db
+//         is PayDb db
 //             ? Results.Ok(db)
 //             : Results.NotFound());
 
-// app.MapPost("/todoitems", async (Pay pay, PayDb db) =>
-// {
-//     db.Pays.Add(pay);
-//     await db.SaveChangesAsync();
+app.MapPost("/todoitems", async (Pay pay, PayDb db) =>
+{
+    db.Pays.Add(pay);
+    await db.SaveChangesAsync();
 
-//     return Results.Created($"/todoitems/{pay.Id}", pay);
-// });
+    return Results.Created($"/todoitems/{pay.Id}", pay);
+});
 
 
 
