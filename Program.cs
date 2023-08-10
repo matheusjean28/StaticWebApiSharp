bdf2bb380236d4ac2d642789b61b3c5c7f687138
matheusjean28 <matheusjean28@gmail.com>
2023-08-09 22:07:32 -0300

    <All files> (<All files>) : <the programs was not running on my another pc, I solve the problem>

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ContextPay.PayDb;
+using Models.Pay;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PayDb>(opt => opt.UseInMemoryDatabase("TodoList"));
