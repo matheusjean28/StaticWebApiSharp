b1e892913fb54d6382a952786505a14df12e15bc
matheusjean28 <matheusjean28@gmail.com>
2023-08-10 20:57:43 -0300

    <Feat> <Program.cs> : <PUT now is ok, we can update some payment on list>

--- Models/PayModel.cs
+++ Models/PayModel.cs
@@ -4,4 +4,6 @@ public class Pay
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+    
+ 
 }
--- Program.cs
+++ Program.cs
@@ -7,26 +7,40 @@ builder.Services.AddDbContext<PayDb>(opt => opt.UseInMemoryDatabase("TodoList"))
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-app.MapGet("/todoitems", async (PayDb db) =>
+app.MapGet("/paylist", async (PayDb db) =>
     await db.Pays.ToListAsync());
 
-app.MapGet("/todoitems/complete", async (PayDb db) =>
+app.MapGet("/paylist/complete", async (PayDb db) =>
     await db.Pays.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/todoitems/{id}", async (int id, PayDb db) =>
+app.MapGet("/paylist/{id}", async (int id, PayDb db) =>
     await db.Pays.FindAsync(id)
         is Pay pay
             ? Results.Ok(pay)
-            : Results.NotFound());
+            : Results.NotFound($"sorry, item {id} not found"));
 
-app.MapPost("/todoitems", async (Pay pay, PayDb db) =>
+app.MapPost("/paylist", async (Pay pay, PayDb db) =>
 {
     db.Pays.Add(pay);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/todoitems/{pay.Id}", pay);
+    return Results.Created($"/paylist/{pay.Id}", pay);
 });
 
+
+app.MapPut("/paylist/{id}", async (int id, Pay pay, PayDb db) =>
+{
+    var _pay = await db.Pays.FindAsync(id);
+
+    if (_pay is null) return Results.NotFound($"Your Payment was not found!");
+
+    _pay.Name = pay.Name;
+    _pay.IsComplete = pay.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok($"Your Paymente was Updated: {_pay.Name}");
+});
 
 
 app.Run();
