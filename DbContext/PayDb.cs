5d3ab6cfa496d5acd7ae56824b9e135ca6ba8d09
matheusjean28 <matheusjean28@gmail.com>
2023-08-11 21:11:30 -0300

    relationships error in  FK

--- DbContext/PayDb.cs
+++ DbContext/PayDb.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.EntityFrameworkCore;
 using Models.Pay;
+using Models.Product;
 
 namespace ContextPay.PayDb;
 class PayDb : DbContext
@@ -9,4 +10,19 @@ class PayDb : DbContext
         : base(options) { }
 
     public DbSet<Pay> Pays => Set<Pay>();
+}
+
+class ProductDb : DbContext
+{
+    public ProductDb(DbContextOptions<ProductDb> options)
+     : base(options) { }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Pay> Pays { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .HasOne(p => p.Pay)
+            .WithMany(p => p.Products)
+            .HasForeignKey(p => p.PayId);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,44 +1,50 @@
 using Microsoft.EntityFrameworkCore;
 using ContextPay.PayDb;
 using Models.Pay;
+using Models.Product;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PayDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-app.MapGet("/paylist", async (PayDb db) =>
+app.MapGet("/v1/paylist", async (PayDb db) =>
     await db.Pays.ToListAsync());
 
-app.MapGet("/paylist/complete", async (PayDb db) =>
+//get prod
+app.MapGet("/v1/product", async (ProductDb db) =>
+    await db.Products.ToListAsync());
+
+app.MapGet("/v1/paylist/complete", async (PayDb db) =>
     await db.Pays.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/paylist/{id}", async (int id, PayDb db) =>
+app.MapGet("/v1/paylist/{id}", async (int id, PayDb db) =>
     await db.Pays.FindAsync(id)
         is Pay pay
             ? Results.Ok(pay)
             : Results.NotFound($"sorry, item {id} not found"));
 
 
-app.MapPost("/paylist", async (Pay pay, PayDb db) =>
+app.MapPost("/v1/paylist", async (Pay pay, PayDb db) =>
 {
     db.Pays.Add(pay);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/paylist/{pay.Id}", pay);
+    return Results.Created($"/v1/paylist/{pay.Id}", pay);
 });
+
 
 // add some produt
-app.MapPost("/paylist", async (Pay pay, PayDb db) =>
-{
-    db.Pays.Add(pay);
+app.MapPost("/v1/product", async (Product product, ProductDb db) =>
+{   
+    db.Products.Add(product);
     await db.SaveChangesAsync();
-
-    return Results.Created($"/paylist/{pay.Id}", pay);
+    return Results.Created($"/v1/product/{product.Id}", product);
 });
 
 
-app.MapPut("/paylist/{id}", async (int id, Pay pay, PayDb db) =>
+app.MapPut("/v1/paylist/{id}", async (int id, Pay pay, PayDb db) =>
 {
     var _pay = await db.Pays.FindAsync(id);
 
