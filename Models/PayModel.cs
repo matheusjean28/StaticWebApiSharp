8af67402d5ce3faad87cde3956f9b0f7c08335ba
matheusjean28 <matheusjean28@gmail.com>
2023-08-13 15:12:39 -0300

    Now working in Database branch

--- Models/PayModel.cs
+++ Models/PayModel.cs
@@ -1,12 +1,15 @@
 using ModelsProduct;
 
-namespace ModelsPay;
-public class Pay
+namespace ModelsPay
 {
-    public int Id { get; set; }
-    public string? Name { get; set; } = string.Empty;
-    public bool IsComplete { get; set; } = false;
 
-    public List<Product> Products { get; set; } = new List<Product>();
+    public class Pay
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public bool IsComplete { get; set; } = false;
 
+        public List<Product> Products { get; set; } = new List<Product>();
+
+    }
 }
--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -1,13 +1,17 @@
 
 using ModelsPay;
-namespace ModelsProduct;
-public class Product
+namespace ModelsProduct
 {
-    public int Id { get; set; }
-    public string Owner { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-    public bool IsComplete { get; set; }
-    public required int PayId { get; set; }
-    public required Pay? Pay { get; set; } = null!;
 
+    public class Product
+    {
+            public int Id { get; set; }
+        public string Owner { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public bool IsComplete { get; set; }
+
+        public int PayId { get; set; } 
+        public Pay? Pay { get; set; } 
+
+    }
 }
